ea331cb28572f8f115d742c90c0c99f951716ce1
Rohan Murmu <scythrine05@gmail.com>
2025-10-14 00:53:24 +0530

    🔔 update: Notify Dept controllers and Respective major section depots

---
+++ src/data/store.js
@@ -0,0 +1,16 @@
+export const MajorSectionDepot = {
+    "MAS-GDR": ["BBQ", "TVT", "PON", "SPE", "ADEE/PON", "MAS", "GPD", "NYP", "GDR", "BRIDGE"],
+    "MAS-AJJ": [
+        "BBQ",
+        "TRL",
+        "AJJ",
+        "AJJE",
+        "MSB",
+        "WSTA",
+        "WSTB",
+        "AVD",
+        "TRLA",
+        "TRLB",
+        "BRIDGE",
+    ],
+};

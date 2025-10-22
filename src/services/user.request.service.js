ef21b7eb39645bbe5f286157d872512c50e8f886
Rohan Murmu <scythrine05@gmail.com>
2025-10-22 15:14:10 +0530

    🔨 fix : Small error fix

--- src/services/user.request.service.js
+++ src/services/user.request.service.js
@@ -255,7 +255,7 @@ export const createRequest = async (data, userId, divisionCode) => {
                     status: filteredData.isSanctioned ? "APPROVED" : "PENDING",
                     divisionId,
                     overAllStatus,
-                    createdAt: istNow,
+                    createdAt: now,
                 },
             });
 

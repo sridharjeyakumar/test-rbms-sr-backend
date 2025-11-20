dda2a92a191c09d89d0e2242c1781329a079a850
Sridharjeyakumar <sridhardk7@gmail.com>
2025-11-20 19:45:16 +0530

    working on user gr report

--- src/services/user.request.service.js
+++ src/services/user.request.service.js
@@ -646,7 +646,7 @@ export const createRequest = async (data, userId, divisionCode) => {
                     status: filteredData.isSanctioned ? "APPROVED" : "PENDING",
                     divisionId,
                     overAllStatus,
-                    createdAt: now,
+                    createdAt: istNow,
                 },
             });
 
